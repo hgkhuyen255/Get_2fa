a4112d5f8ce4a8b7813d3c7476d2b946f88f3c17
hgkhuyen255 <hgkhuyen678@gmail.com>
2025-04-14 02:55:51 +0700

    Add files via upload

--- webhook_setup.py
+++ webhook_setup.py
@@ -2,11 +2,11 @@ import os
 import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-RAILWAY_URL = os.getenv("RAILWAY_URL")  # Domain Railway ví dụ: https://your-bot.up.railway.app/
+RAILWAY_URL = os.getenv("RAILWAY_URL")  # Đã bao gồm /webhook trong URL
 
 def set_webhook():
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook"
-    webhook_url = f"{RAILWAY_URL}/"
+    webhook_url = RAILWAY_URL
     response = requests.get(url, params={"url": webhook_url})
     print("Webhook response:", response.json())
 
